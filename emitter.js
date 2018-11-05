f5434dbaf0ee6fb37c06384fef2b52d5b7c57a5e
frosthamster <frosthamster@gmail.com>
2018-11-05 23:44:16 +0500

    _

--- emitter.js
+++ emitter.js
@@ -6,11 +6,81 @@
  */
 const isStar = true;
 
+/*
+ох, как же его не хватает
+*/
+function defaultDict(factory) {
+    const dict = new Map();
+
+    const _get = dict.get.bind(dict);
+    dict.get = function (key) {
+        if (!dict.has(key)) {
+            dict.set(key, factory());
+        }
+
+        return _get(key);
+    };
+
+    return dict;
+}
+
+
+class Callback {
+    constructor(context, handler, predicate) {
+        this.context = context;
+        this.handler = handler;
+        this.predicate = predicate;
+        this.runsCount = 0;
+    }
+
+    run() {
+        if (this.predicate(this.runsCount++)) {
+            this.handler.apply(this.context);
+        }
+    }
+}
+
+
+class Event {
+    constructor() {
+        this._listeners = defaultDict(() => []);
+    }
+
+    add(callback) {
+        this._listeners.get(callback.context).push(callback);
+    }
+
+    removeListener(listenerId) {
+        this._listeners.set(listenerId, []);
+    }
+
+    runCallbacks() {
+        for (let callbacks of this._listeners.values()) {
+            for (let callback of callbacks) {
+                callback.run();
+            }
+        }
+    }
+}
+
+
+function* prefixes(event, delimiter = '.') {
+    let result = event.split(delimiter);
+
+    while (result.length > 0) {
+        yield result.join(delimiter);
+        result.pop();
+    }
+}
+
+
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    const events = defaultDict(() => new Event());
+
     return {
 
         /**
@@ -18,26 +88,38 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @param {Function} predicate - additional condition for event
+         * @returns {ThisType}
          */
-        on: function (event, context, handler) {
-            console.info(event, context, handler);
+        on: function (event, context, handler, predicate = () => true) {
+            events.get(event).add(new Callback(context, handler, predicate));
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {ThisType}
          */
         off: function (event, context) {
-            console.info(event, context);
+            events.get(event).removeListener(context);
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {ThisType}
          */
         emit: function (event) {
-            console.info(event);
+            for (let subEvent of prefixes(event)) {
+                events.get(subEvent).runCallbacks();
+            }
+
+            return this;
         },
 
         /**
@@ -47,9 +129,10 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {ThisType}
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            return this.on(event, context, handler, (runsCount) => runsCount < times);
         },
 
         /**
@@ -59,9 +142,10 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {ThisType}
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            return this.on(event, context, handler, (runsCount) => runsCount % frequency === 0);
         }
     };
 }
